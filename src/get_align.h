e9cc0bee1396b4b0b7907869f1e4361646b5d002
ssssssiyu <zhousiyut@gmail.com>
2020-04-24 23:07:35 -0400

    Delete get_align.h

--- src/get_align.h
+++
@@ -1,7 +0,0 @@
-#ifndef get_align_H
-#define get_align_H
-
-void get_align(char seq1[], char seq2[], int match_score, int mismatch_score,
-	int gap_penalty, char* seq1_align,char* seq2_align,int scores[50][50]);
-
-#endif//get_align_H
